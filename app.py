4b81cd97d07f2a31037ec633ddb8437bd7c3d854
HaileyMa-wk <wkmaad@connect.ust.hk>
2026-04-24 12:40:44 +0800

    Update app.py

--- app.py
+++ app.py
@@ -14,11 +14,18 @@ st.write(f"Your age is {age}")
 option = st.selectbox("Choose your favorite color:",
                       ["Red", "Blue", "Green"])
 st.write(f"You selected: {option}")
-if st.button("Click Me"):
-    st.write("Button clicked!")
-else:
-  st.write("why you dont click???")
+if "click_count" not in st.session_state:
+    st.session_state.click_count = 0
 
+if st.button("Click Me"):
+    st.session_state.click_count += 1
+
+if st.session_state.click_count == 1:
+    st.write("Button clicked!")
+elif st.session_state.click_count >= 2:
+    st.write("click too much!")
+else:
+    st.write("why you dont click???")
 st.success("Operation completed successfully!")
 st.write("Operation completed successfully!")
 st.header("Operation completed successfully!")
