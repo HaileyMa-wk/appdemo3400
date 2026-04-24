13069ad20e0ec1546ea2b8bcea51ff6468d626db
HaileyMa-wk <wkmaad@connect.ust.hk>
2026-04-24 12:28:53 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,5 @@ import streamlit as st
 
 st.title("Welcome to Streamlit")
 
-st.write("Section 1: Introduction")
-st.write("Hello, Streamlit")
-st.write(12345)
-st.write({"Name":"Alice","Age":30})
-st.write(['list1','list2'])
-
+st.header("ISOM3400")
+st.write("**Bold Text** and *Italic Text*")
